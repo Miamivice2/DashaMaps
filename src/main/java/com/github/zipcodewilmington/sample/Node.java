07be9570ad99bf679a1e9f6914dd91a513040eae
Miamivice2 <andreb.davis2@gmail.com>
2023-04-07 17:03:10 -0400

    making tests for dash maps

---
+++ src/main/java/com/github/zipcodewilmington/sample/Node.java
@@ -0,0 +1,33 @@
+package com.github.zipcodewilmington.sample;
+
+public class Node {
+
+    public class Node {
+        String value;
+        String key;
+        Node next;
+
+        public Node(String data) {
+            this.value = data;
+            key = null;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+            this.key
+        }
+
+        public Node getKey() {
+            return key;
+        }
+
+        public void setNext( Node) {
+            this.key = Node;
+        }
+
+
+        public Object getValue() {
+        }
+    }
+
+}
--- src/test/java/com/github/zipcodewilmington/DashaMapTest.java
+++ src/test/java/com/github/zipcodewilmington/DashaMapTest.java
@@ -1,9 +1,73 @@
 package com.github.zipcodewilmington;
 
+import com.fasterxml.jackson.databind.deser.CreatorProperty;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author xtofer
  * @version 1.0.0
  * @date 10/21/19 9:05 AM
  */
-public class DashaMapTest {
+public class DashaMap1Test {
+}
+
+    @Test
+    public void before() {
+
+    map = new DashaMap();
+    }
+
+    @Test
+    public void setTest(){
+    String key = "key";
+    String value = "value";
+
+    map.set(key, value);
+    long actual = map.bucketsize(key "k");
+
+        
+    }
+
+    @Test
+    public void deleteTest() {
+        String key = "key";
+        String value = "value";
+        long expected = 0;
+        map.set(key, value);
+        map.delete(key);
+        long actual = map.bucketSize
+
+
+    }
+
+    @Test
+    public void getTest() {
+    String key = "ki";
+    String expected = "energy";
+    map.set(key, expected);
+    String actual = map.get(expected);
+
+    Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void isEmptyTest() {
+    String e  = "e";
+    String key = "dre";
+    map.set(key,e);
+    Assert.assertEquals(map.isEmpty());
+    }
+
+    @Test
+    public void isEmptyTest2() {
+    Assert.assertEquals(map.isEmpty());
+
+
+    }
+
+    @Test
+    public void name() {
+    }
 }
